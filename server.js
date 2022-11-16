c4eec2e564e3aa70eb17f448d58f35932a477d15
Dominick Tolomeo <tolomeodom@gmail.com>
2022-11-15 16:55:21 -0800

    basic router started

---
+++ server.js
@@ -0,0 +1,40 @@
+/*
+ * Temp file to load pages
+ * Use "npm start" in home repo to start program in production
+ * Use "npm dev" to run in developer mode (automatic refresh)
+ * - Dom
+ */
+
+var http = require('http')
+var fs = require('fs')
+var css = fs.readFileSync('./public/style.css', 'utf8')
+var js = fs.readFileSync('./public/index.js', 'utf8')
+var html = fs.readFileSync('./public/index.html', 'utf8')
+
+var server = http.createServer(function(req,res) {
+    console.log("request recieved. URL:", req.url)
+    if (req.url === "/" || req.url === "/index.html") {
+        res.statusCode = 200
+        res.statusMessage = "Success"
+        res.setHeader('content-type', 'text/html')
+        res.write(html)
+    }
+    else if (req.url === "/style.css") {
+        res.statusCode = 200
+        res.statusMessage = "Success"
+        res.setHeader('content-type', 'text/css')
+        res.write(css)
+    }
+    else if (req.url === "/index.js") {
+        res.statusCode = 200
+        res.statusMessage = "Success"
+        res.setHeader('content-type', 'application/javascript')
+        res.write(js)
+    }
+})
+
+
+var port = (process.env.PORT === undefined)? 3000 : process.env.PORT
+server.listen(port, function () {
+    console.log("server is listening on port", port)
+})
